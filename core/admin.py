2e47350d0236710eb47bd4b9b980841c79253da2
Ankan Poddar <friendyankan@gmail.com>
2018-04-03 13:14:20 +0530

    Add the repository model
    Add the model Repository to store the repositories of a user when
    the UserProfile object is created and add a function to listen to
    the post_save signal and store the repositories.

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from core.models import UserProfile
+from core.models import UserProfile, Repository
 
 
 admin.site.register(UserProfile)
+admin.site.register(Repository)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
@@ -12,3 +14,33 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Repository(models.Model):
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    repo = models.CharField(max_length=100)
+    is_fork = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Repositories'
+
+    def __str__(self):
+        return self.repo
+
+
+@receiver(post_save, sender=UserProfile)
+def save_repositories(sender, instance, created, **kwargs):
+    """Save the repositories of a user when its UserProfile object is saved."""
+    if created:
+        username = instance.user.username
+        repos_url = 'https://api.github.com/users/{}/repos'.format(username)
+        response = requests.get(repos_url)
+        repos = []
+        if response.status_code == 200:
+            repos = response.json()
+        for repo in repos:
+            Repository.objects.create(
+                owner=instance,
+                repo=repo['full_name'],
+                is_fork=repo['fork']
+            )
