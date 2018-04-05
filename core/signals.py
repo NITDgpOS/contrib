c01bd3acabb2500331428ac6d18f463703d8c838
Ankan Poddar <friendyankan@gmail.com>
2018-04-06 00:00:50 +0530

    Change save_repositories function to complete_signup in signals.py
    Alongwith saving the repositories of the user, the function now
    also updates the contribution count of the user as soon as he
    signs up.

--- core/signals.py
+++ core/signals.py
@@ -2,12 +2,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from core.models import UserProfile, Repository
-from core.utils import get_repositories
+from core.utils import get_repositories, update_contributions
 
 
 @receiver(post_save, sender=UserProfile)
-def save_repositories(sender, **kwargs):
-    """Save the repositories of a user when its UserProfile object is saved."""
+def complete_signup(sender, **kwargs):
+    """Save the repositories of a user when its UserProfile object is saved and
+    update his contributions."""
     if kwargs['created']:
         username = kwargs['instance'].user.username
         repos = get_repositories(username)
@@ -17,3 +18,4 @@ def save_repositories(sender, **kwargs):
                 repo=repo['full_name'],
                 is_fork=repo['fork']
             )
+        update_contributions(kwargs['instance'])
